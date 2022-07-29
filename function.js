b218a7adbb683d7bb81034c1a8a150923dd5ef99
Yuexing Hao <yh727@cornell.edu>
2022-07-28 22:33:59 -0400

    Update function.js

--- function.js
+++ function.js
@@ -130,28 +130,54 @@ function O_Search() {
 }
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// Get the modal for full description
+var full_modal = document.getElementById("full_Modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var full_btn = document.getElementById("full_Btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var full_span = document.getElementsByClassName("full_close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+full_btn.onclick = function() {
+  full_modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+full_span.onclick = function() {
+  full_modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == full_modal) {
+    full_modal.style.display = "none";
   }
 }
+
+// Get the modal for full description
+var one_modal = document.getElementById("one_Modal");
+
+// Get the button that opens the modal
+var one_btn = document.getElementById("one_Btn");
+
+// Get the <span> element that closes the modal
+var one_span = document.getElementsByClassName("one_close")[0];
+
+// When the user clicks the button, open the modal 
+one_btn.onclick = function() {
+  one_modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+one_span.onclick = function() {
+  one_modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == one_modal) {
+    one_modal.style.display = "none";
+  }
+}
+
+
