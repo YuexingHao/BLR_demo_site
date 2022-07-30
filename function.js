a1a892852c7ab96d96df188af5387b4edea532dd
Yuexing Hao <yh727@cornell.edu>
2022-07-30 12:54:25 -0400

    Update function.js

--- function.js
+++ function.js
@@ -130,6 +130,22 @@ function O_Search() {
 }
 
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
 // Get the modal for full description
 var full_modal = document.getElementById("full_Modal");
 
